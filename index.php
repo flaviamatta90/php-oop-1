262b099a31f5b2842e1b68b503941ae5b74f46d6
Flavia <flaviamatta90@gmail.com>
2020-10-22 15:46:38 +0200

    user

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+class User {
+    public $name;
+    public $lastname;
+    public $adress;
+    public $phoneNumber;
+    public $email;
+    public $password;
+}
+
+$user1 = new User();
+    $user1->name = "Flavia";
+    $user1->lastname = "Matta";
+    $user1 ->email= "ssskf.gmail.com";
+    $user1->password = "123456";
+
+$user2 = new User();
+    $user2->name = "Franco";
+    $user2->lastname = "Neri";
+    $user2 ->email= "sfhyj.gmail.com";
+    $user2->password = "789101";
+
+$user3 = new User();
+    $user2->name = "Bianca";
+    $user2->lastname = "Verdi";
+    $user2 ->email= "ooiuy.gmail.com";
+    $user2->password = "112131";
+
+$user4 = new User();
+    $user2->name = "Mario";
+    $user2->lastname = "Rossi";
+    $user2 ->email= "kutg.gmail.com";
+    $user2->password = "415161";
+
+$user5 = new User();
+    $user2->name = "Aldo";
+    $user2->lastname = "Gialli";
+    $user2 ->email= "werrr.gmail.com";
+    $user2->password = "718192";
